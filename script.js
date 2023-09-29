b7aa4df750e363634a43667991de35a951f0bcc3
EmpireAntz <thebigboognish@gmail.com>
2023-09-29 00:40:19 -0700

    added a prompt to choose number of characters for the password and it will tell you if its going to be too long or too short. if it fits the criteria it will alert you how many characters your password will be

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var userInput = prompt("Choose how Many Characters you want your pasword to be");
+
+  if (userInput < 8) {
+    alert("Password too short! Must be at least 8 characters!");
+    return generatePassword();
+  }
+
+  else if (userInput > 128) {
+    alert("Password is too long! Must be l28 characters or less!");
+    return generatePassword();
+  }
+
+  else {
+     alert("Your password is " + userInput + " characters long");
+  }
+  return "Something"
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +30,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+/*  1. Prompt user to enter password critera
+    2. Password length i < 8 or i > 128
+    3.  If Password doesnt match critera bring back to first prompt input
+    4. Uppercase, lowercase, numbers and special characters
+    5. Input type should be validated
+    6. Password is generated based on the criteria of the prompts
+    7. Display password in alert or on the page
+*/
