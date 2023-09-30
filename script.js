efabf3cf44f315984e32aeb2fb457237547c5516
EmpireAntz <thebigboognish@gmail.com>
2023-09-29 23:55:09 -0700

    added arrays for the numbers, upper, lower, and special characters and created prompts and confims to accept criteria for the pasword. Next is to add a function that takes in criteria and outputs based on the criteria met.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var uppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialArr = ["!","@","#","$","^","&","*","(",")","?"];
 
 function generatePassword() {
-  var userInput = prompt("Choose how Many Characters you want your pasword to be");
+  var userInput = prompt("Choose how Many Characters do you want your pasword to be");
 
   if (userInput < 8) {
     alert("Password too short! Must be at least 8 characters!");
@@ -10,14 +14,23 @@ function generatePassword() {
   }
 
   else if (userInput > 128) {
-    alert("Password is too long! Must be l28 characters or less!");
+    alert("Password is too long! Must be 128 characters or less!");
+    return generatePassword();
+  }
+
+  else if (isNaN(userInput)) {
+    alert(userInput + " is not a number!");
     return generatePassword();
   }
 
   else {
-     alert("Your password is " + userInput + " characters long");
+    alert("Your password will be " + userInput + " characters long");
+    var userNumConf = confirm("Would you like to add numbers?");
+    var userUpperConf = confirm("Would you like to add uppercase letters?");
+    var userLowerConf = confirm("Would you like to add lowercase letters?");
+    var userSpecialConf = confirm("Would you like to add special characters?");
+    console.log(userNumConf, userUpperConf, userLowerConf, userSpecialConf);
   }
-  return "Something"
 }
 // Write password to the #password input
 function writePassword() {
@@ -35,7 +48,7 @@ generateBtn.addEventListener("click", writePassword);
 
 /*  1. Prompt user to enter password critera
     2. Password length i < 8 or i > 128
-    3.  If Password doesnt match critera bring back to first prompt input
+    3. If Password doesnt match critera bring back to first prompt input
     4. Uppercase, lowercase, numbers and special characters
     5. Input type should be validated
     6. Password is generated based on the criteria of the prompts
